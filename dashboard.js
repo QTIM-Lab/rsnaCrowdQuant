33907d8bdb7ba8691b3a9fcf6352d8d52a0601ca
alan.cramer <alan.cramer@hmhco.com>
2017-12-03 15:34:24 -0500

    color update, anno time histogram refinement

--- dashboard.js
+++ dashboard.js
@@ -339,7 +339,7 @@ function populateAnnotationsPerAnnotator(annoToCatToCt) {
         .rangeRound([height, 0]);
 
     var z = d3.scaleOrdinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+        .range(["#ffffb2", "#fecc5c", "#fd8d3c", "#f03b20", "#bd0026"]);
 
     var data = Object.keys(annoToCatToCt).map(function (a) { return annoToCatToCt[a]; });
 
